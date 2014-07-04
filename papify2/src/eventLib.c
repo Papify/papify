9057620b9ea6a19f9957f37de4dde497696a7591
panacotta <alejoar@gmail.com>
2014-07-04 20:14:53 +0200

    back to first relese

--- papify2/src/eventLib.c
+++ papify2/src/eventLib.c
@@ -6,7 +6,7 @@ int eventSet1 = PAPI_NULL;
 int * eventSet;
 
 
-//int tot al1 = 0;						   /* total overflows */
+//int total1 = 0;						   /* total overflows */
 //int total2 = 0;						   /* total overflows */
 int total = 0;
 
